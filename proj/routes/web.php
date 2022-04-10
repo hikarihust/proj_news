7f30b6f5a4f23cc8e4630f9e9562e6e7194f3248
vudinhquang <vudinhquangk53@gmail.com>
2022-04-10 17:01:56 +0900

    Router

--- proj/routes/web.php
+++ proj/routes/web.php
@@ -11,6 +11,57 @@
 |
 */
 
+$prefixAdmin = Config::get('zvn.url.prefix_admin', 'admin');
+
+/*
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('category/{id}', function ($id) {
+    return 'Category '.$id;
+})->where('id', '[0-9]+');
+
+Route::get('category/{name?}', function ($name = 'John') {
+    return $name;
+});
+
+Route::group(['prefix'=> $prefixAdmin], function () {
+    Route::get('user', function () {
+        return "/admin/user";
+    });
+    Route::get('slider', function () {
+        return "/admin/slider";
+    });
+    Route::get('category', function () {
+        return "/admin/category";
+    });
+});
+
+*/
+
+Route::prefix($prefixAdmin)->group(function () {
+    Route::get('user', function () {
+        return "/admin/user";
+    });
+
+    Route::prefix('slider')->group(function () {
+        Route::get('', function () {
+            return "slider List";
+        });
+        Route::get('edit/{id}', function ($id) {
+            return "slide edit" . $id;
+        })->where('id', '[0-9]+');
+        Route::get('delete/{id}', function ($id) {
+            return "slide delete" . $id;
+        })->where('id', '[0-9]+');
+    });
+
+    Route::get('category', function () {
+        return "/admin/category";
+    });
 });
