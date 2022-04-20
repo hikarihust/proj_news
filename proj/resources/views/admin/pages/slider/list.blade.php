c1b78310bf747d05fc05a203e43a280233a416d0
vudinhquang <vudinhquangk53@gmail.com>
2022-04-20 10:53:02 +0900

    foreach

--- proj/resources/views/admin/pages/slider/list.blade.php
+++ proj/resources/views/admin/pages/slider/list.blade.php
@@ -13,7 +13,43 @@
             </thead>
             <tbody>
                 @if (count($items) > 0)
-                    data
+                    @foreach ($items as $key => $val)
+                        @php
+                            $index = $key + 1;
+                            $class = $index % 2 === 0 ? 'even' : 'odd';
+                            $id = $val['id'];
+                            $name = $val['name'];
+                            $description = $val['description'];
+                            $link = $val['link'];
+                            $thumb = $val['thumb'];
+                            $status = $val['status'];
+                            $createdHistory = $val['created_by'];
+                            $modifiedHistory = $val['modified_by'];
+                        @endphp
+                        <tr class="{{ $class }} pointer">
+                            <td> {{ $index }} </td>
+                            <td width="40%">
+                                <p><strong>Name:</strong> {!! $name !!} </p>
+                                <p><strong>Description:</strong>{!! $description !!}</p>
+                                <p><strong>Link:</strong>{!! $link !!}</p>
+                                <p><img src="{{ $name }}" alt="{{ $name }}" class="zvn-thumb"></p>
+                            </td>
+                            <td>{!! $status !!}</td>
+                            <td>{!! $createdHistory !!}</td>
+                            <td>{!! $modifiedHistory !!}</td>
+                            <td class="last">
+                                <div class="zvn-box-btn-filter"><a href="/form/1" type="button"
+                                        class="btn btn-icon btn-success" data-toggle="tooltip" data-placement="top"
+                                        data-original-title="Edit">
+                                        <i class="fa fa-pencil"></i>
+                                    </a><a href="/delete/1" type="button" class="btn btn-icon btn-danger btn-delete"
+                                        data-toggle="tooltip" data-placement="top" data-original-title="Delete">
+                                        <i class="fa fa-trash"></i>
+                                    </a>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
                 @else
                     @include('admin.templates.list_empty', ['colspan' => 6])
                 @endif
