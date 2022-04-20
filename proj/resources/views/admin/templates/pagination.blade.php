c9a00f3da3538af2da349b2c8b622c6cc7c6e960
vudinhquang <vudinhquangk53@gmail.com>
2022-04-21 00:31:43 +0900

    custom view

--- proj/resources/views/admin/templates/pagination.blade.php
+++ proj/resources/views/admin/templates/pagination.blade.php
@@ -1,24 +1,14 @@
 <div class="x_content">
     <div class="row">
         <div class="col-md-6">
-            <p class="m-b-0">Số phần tử trên trang: <b>2</b> trên <span
-                    class="label label-success label-pagination">3 trang</span></p>
-            <p class="m-b-0">Hiển thị<b> 1 </b> đến<b> 2</b> trên<b> 6</b> Phần tử</p>
+            <p class="m-b-0">
+                <span class="label label-info label-pagination">{{ $items->perPage() }} items per page</span>
+                <span class="label label-success label-pagination">{{ $items->total() }} items</span>
+                <span class="label label-danger label-pagination">{{ $items->lastPage() }} pages</span>
+            </p>
         </div>
         <div class="col-md-6">
-            <nav aria-label="Page navigation example">
-                <ul class="pagination zvn-pagination">
-                    <li class="page-item disabled">
-                        <a class="page-link" href="#" tabindex="-1">«</a>
-                    </li>
-                    <li class="page-item"><a class="page-link" href="#">1</a></li>
-                    <li class="page-item active"><a class="page-link" href="#">2</a></li>
-                    <li class="page-item"><a class="page-link" href="#">3</a></li>
-                    <li class="page-item">
-                        <a class="page-link" href="#">»</a>
-                    </li>
-                </ul>
-            </nav>
+            {!! $items->links('pagination.pagination_backend') !!}
         </div>
     </div>
 </div>
