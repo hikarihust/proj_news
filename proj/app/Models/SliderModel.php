fd9e5d1fecac9229539df5b039516e92af5963ad
vudinhquang <vudinhquangk53@gmail.com>
2022-04-25 00:33:18 +0900

    AdminModel

--- proj/app/Models/SliderModel.php
+++ proj/app/Models/SliderModel.php
@@ -2,32 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use App\Models\AdminModel;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use DB;
 
-class SliderModel extends Model
+class SliderModel extends AdminModel
 {
-    protected $table = 'slider';
-    protected $primaryKey ='id';
-    const CREATED_AT = 'created';
-    const UPDATED_AT = 'modified';
-    protected $folderUpload = '';
-    protected $fieldsearchAccepted = [
-        'id',
-        'name',
-        'description',
-        'link'
-    ];
-
-    protected $crudNotAccepted = [
-        '_token',
-        'thumb_current',
-    ];
-
     public function __construct() {
+        $this->table = 'slider';
         $this->folderUpload = 'slider';
+        $this->fieldsearchAccepted = ['id', 'name', 'description', 'link'];
     }
 
     public function listItems($params = null, $options = null){
@@ -123,20 +108,5 @@ class SliderModel extends Model
             $this->_deleteThumb($item['thumb']);
             $this->where('id', $params['id'])->delete();
         }
-    }
-
-    protected function _prepareParams($params) {
-        return $params = array_diff_key($params, array_flip($this->crudNotAccepted));
-    }
-
-    protected function _uploadThumb($thumbObj) {
-        $thumbName = Str::random(10) . '.' . $thumbObj->clientExtension();
-        $thumbObj->storeAs($this->folderUpload, $thumbName, 'zvn_storage_image');
-
-        return $thumbName;
-    }
-
-    protected function _deleteThumb($thumbName) {
-        Storage::disk('zvn_storage_image')->delete($this->folderUpload . '/' . $thumbName);
     }
 }
