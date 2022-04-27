b34fbdbd6bb083fc1b65a6cb24973760970bcb60
vudinhquang <vudinhquangk53@gmail.com>
2022-04-27 23:37:45 +0900

    show slider

--- proj/resources/views/news/block/slider.blade.php
+++ proj/resources/views/news/block/slider.blade.php
@@ -1,60 +1,43 @@
-<div class="home">
-    <!-- Home Slider -->
-    <div class="home_slider_container">
-        <div class="owl-carousel owl-theme home_slider">
-            <!-- Slide -->
-            <div class="owl-item home_slider_item">
-                <div class="background_image"
-                    style="background-image:url({{ asset('news/images/zendvn-frontend-master2.jpg') }})"></div>
-                <div class="home_slider_content text-center">
-                    <div class="home_slider_content_inner" data-animation-in="fadeIn"
-                        data-animation-out="animate-out fadeOut">
-                        <div class="home_category"><a href="category.html">technology</a></div>
-                        <div class="home_title">Khóa học lập trình Frontend Master</div>
-                        <div class="home_text">Khóa học sẽ giúp bạn trở thành một chuyên gia Frontend với đầy đủ các
-                            kiến thức về HTML, CSS, JavaScript, Bootstrap, jQuery, chuyển PSD thành HTML ...
+@if (count($itemsSlider) > 0)
+    <div class="home">
+        <!-- Home Slider -->
+        <div class="home_slider_container">
+            <div class="owl-carousel owl-theme home_slider">
+                <!-- Slide -->
+                @foreach ($itemsSlider as $key => $val)
+                    @php
+                        $name = $val['name'];
+                        $description = $val['description'];
+                        $link = $val['link'];
+                        $thumb = url('/images/slider') . '/' . $val['thumb'];
+                    @endphp
+                    <div class="owl-item home_slider_item">
+                        <div class="background_image"
+                            style="background-image:url({{ asset('news/images/zendvn-frontend-master2.jpg') }})">
                         </div>
-                        <div class="home_button trans_200"><a href="#">read more</a></div>
-                    </div>
-                </div>
-            </div>
-            <!-- Slide -->
-            <div class="owl-item home_slider_item">
-                <div class="background_image"
-                    style="background-image:url({{ asset('news/images/zendvn-hoclaptrinhonline.jpg') }})"></div>
-                <div class="home_slider_content text-center">
-                    <div class="home_slider_content_inner" data-animation-in="fadeIn"
-                        data-animation-out="animate-out fadeOut">
-                        <div class="home_category"><a href="category.html">technology</a></div>
-                        <div class="home_title">Học lập trình trực tuyến</div>
-                        <div class="home_text">Học trực tuyến giúp bạn tiết kiệm chi phí, thời gian, cập nhật được
-                            nhiều kiến thức mới nhanh nhất và hiệu quả nhất
+                        <div class="home_slider_content text-center">
+                            <div class="home_slider_content_inner" data-animation-in="fadeIn"
+                                data-animation-out="animate-out fadeOut">
+                                <div class="home_category"><a href="category.html">technology</a></div>
+                                <div class="home_title">Khóa học lập trình Frontend Master</div>
+                                <div class="home_text">Khóa học sẽ giúp bạn trở thành một chuyên gia Frontend với
+                                    đầy đủ
+                                    các
+                                    kiến thức về HTML, CSS, JavaScript, Bootstrap, jQuery, chuyển PSD thành HTML ...
+                                </div>
+                                <div class="home_button trans_200"><a href="#">read more</a></div>
+                            </div>
                         </div>
-                        <div class="home_button trans_200"><a href="#">read more</a></div>
                     </div>
-                </div>
+                @endforeach
             </div>
-            <!-- Slide -->
-            <div class="owl-item home_slider_item">
-                <div class="background_image"
-                    style="background-image:url({{ asset('news/images/zendvn-uudaihocphi-001.jpg') }})"></div>
-                <div class="home_slider_content text-center">
-                    <div class="home_slider_content_inner" data-animation-in="fadeIn"
-                        data-animation-out="animate-out fadeOut">
-                        <div class="home_category"><a href="category.html">technology</a></div>
-                        <div class="home_title">Ưu đãi học phí</div>
-                        <div class="home_text">Tổng hợp các trương trình ưu đãi học phí hàng tuần, hàng tháng đến
-                            tất các các bạn với các mức giảm đặc biệt 50%, 70%,..
-                        </div>
-                        <div class="home_button trans_200"><a href="#">read more</a></div>
-                    </div>
-                </div>
-            </div>
+            @if (count($itemsSlider) > 1)
+                <!-- Home Slider Navigation -->
+                <div class="home_slider_nav home_slider_prev trans_200"><i class="fa fa-angle-left trans_200"
+                        aria-hidden="true"></i></div>
+                <div class="home_slider_nav home_slider_next trans_200"><i class="fa fa-angle-right trans_200"
+                        aria-hidden="true"></i></div>
+            @endif
         </div>
-        <!-- Home Slider Navigation -->
-        <div class="home_slider_nav home_slider_prev trans_200"><i class="fa fa-angle-left trans_200"
-                aria-hidden="true"></i></div>
-        <div class="home_slider_nav home_slider_next trans_200"><i class="fa fa-angle-right trans_200"
-                aria-hidden="true"></i></div>
     </div>
-</div>
+@endif
