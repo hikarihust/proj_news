0e39c9aa5a3ba5cb0c5aa52f5c507fadf22703bb
vudinhquang <vudinhquangk53@gmail.com>
2022-05-02 16:45:48 +0900

    featured left

---
+++ proj/resources/views/news/partials/article/content.blade.php
@@ -0,0 +1,28 @@
+@php
+use App\Helpers\Template as Template;
+$name = $item['name'];
+$categoryName = $item['category_name'];
+$linkCategory = '#';
+$linkArticle = '#';
+$created = Template::showDatetimeFrontend($item['created']);
+$content = Template::showContent($item['content'], 500);
+@endphp
+<div class="post_content">
+    <div class="post_category cat_technology ">
+        <a href="{{ $linkCategory }}">{{ $categoryName }}</a>
+    </div>
+    <div class="post_title"><a href="{{ $linkArticle }}">{{ $name }}</a>
+    </div>
+    <div class="post_info d-flex flex-row align-items-center justify-content-start">
+        <div class="post_author d-flex flex-row align-items-center justify-content-start">
+            <div>
+                <div class="post_author_image"><img src="{{ asset('news/images/author_1.jpg') }}" alt=""></div>
+            </div>
+            <div class="post_author_name"><a href="#">Quang Vu</a></div>
+        </div>
+        <div class="post_date"><a href="#">{{ $created }}</a></div>
+    </div>
+    <div class="post_text">
+        <p>{!! $content !!}</p>
+    </div>
+</div>
