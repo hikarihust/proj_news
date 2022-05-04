8cd6cef459a79bfaa5dfc07d6b2abef86f293f8f
vudinhquang <vudinhquangk53@gmail.com>
2022-05-05 03:38:23 +0900

    Error 404

---
+++ proj/resources/views/errors/404.blade.php
@@ -0,0 +1,12 @@
+<div class="error-page">
+    <div class="error-page-container d-none d-md-block">
+        <br>
+        <div class="not-found text-center">
+            <p class="not-found-title">NOT FOUND</p>
+            <br><br>
+            <p class="not-found-description">お探しのページは見つかりませんでした</p>
+            <br><br><br>
+            <button class="top-button mt-4" onclick="window.location='{{ route('home') }}'">TOPへ戻る</button>
+        </div>
+    </div>
+</div>
